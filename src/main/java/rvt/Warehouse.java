c00638a64b0923317d007d26312bc7a291eae7ee
24DP1KBerg <a24016kb@rvt.lv>
2026-03-18 11:07:53 +0200

    Part 2 Online Shop

--- src/main/java/rvt/Warehouse.java
+++ src/main/java/rvt/Warehouse.java
@@ -6,10 +6,13 @@ import java.util.Map;
 public class Warehouse {
 
     Map<String, Integer> prices = new HashMap<>();
+    Map<String, Integer> stocks = new HashMap<>();
 
     public void addProduct(String product, int price, int stock){
         prices.put(product, price);
+        stocks.put(product, stock); 
     }
+    
 
     public int price(String product){
 
@@ -17,5 +20,27 @@ public class Warehouse {
             return -99;
         }
         return prices.get(product);
+    }
+
+    public int stock(String product){
+        if (!stocks.containsKey(product)) {
+            return 0;
+        }
+        return stocks.get(product);
+    }
+    public boolean take(String product){
+        if (!prices.containsKey(product)) {
+            return false;
+        }
+        
+        int currentStock = stocks.get(product);
+
+        if (currentStock > 0) {
+            stocks.put(product, currentStock -1 );
+            return true;
+        }
+
+        return false;
+
     }
 }
